1855c3baf75e9c6fbee34e2c4e4610f197c25b0b
HeadMad <kov.ps@ya.ru>
2019-07-30 17:09:08 +0800

    rewrite with new versuin vue-automato

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 const VueAutomato = require('vue-automato')
-const { camelize, capitalize, hyphenate } = require('vue-automato/util')
 
 module.exports = function (prefixes) {
   return new VueAutomato({
-    match (node, opts) {
-      let originalTag = node.tag
-      let kebabTag = hyphenate(originalTag)
-      let camelTag = capitalize(camelize(originalTag))
+    match ({rawTag, camelTag, kebabTag}, params, node, link) {
+      // if the tag is in this file already had
+      if (link[kebabTag]) return false
+      link[kebabTag] = true
 
       for (const prefix in prefixes) {
         if (!kebabTag.startsWith(prefix)) continue
 
-        opts.prefixes = prefixes
-        opts.prefix = prefix
-        opts.name = kebabTag.substring(prefix.length)
-        opts.defaultHandler = require('./lib/default.js')
+        link.prefixes = prefixes
+        link.prefix = prefix
+        link.name = kebabTag.substring(prefix.length)
+        link.defaultHandler = require('./lib/default.js')
         
         const handler = typeof prefixes[prefix] === "function"
           ? prefixes[prefix]
-          : opts.defaultHandler
+          : link.defaultHandler
         
-        return handler({originalTag, kebabTag, camelTag}, opts, node)
+        return handler(...arguments)
       }
     }
   })
